6894966fd5473380e5798e62f583d781c34efa41
amochin <mochinin@gmail.com>
2024-02-06 19:46:29 +0200

    Dynamic API example

--- examples/2_dynamic_api/libs/1_simple_lib.py
+++ examples/2_dynamic_api/libs/1_simple_lib.py
@@ -3,4 +3,16 @@ Implement RF Dynamic Library API according to User Guide:
 https://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#dynamic-library-api
 """
 class MyLib:
-    pass
+    def get_keyword_names(self):
+        my_keywords = ["Get captain",
+                       "Get captains favorite sport"]
+        return my_keywords
+    
+    def run_keyword(self, name, args):
+        print(f"Calling keyword {name} with args {args}")
+
+    def get_keyword_arguments(self, name):
+        if name == "Get captain":
+            return ["Starship"]
+        else:
+            return ["Captains name"]
--- examples/2_dynamic_api/tests/test.robot
+++ examples/2_dynamic_api/tests/test.robot
@@ -3,4 +3,5 @@ Library    examples.2_dynamic_api.libs.1_simple_lib.MyLib
 
 *** Test Cases ***
 T1
-    No Operation
+    Get Captain    Enterprise
+    Get Captains Favorite Sport    Archer
